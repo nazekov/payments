70342b6e29b06b77f7d567b10cb58b690896c00c
nazekov <nazekov.ul@gmail.com>
2023-05-18 07:29:30 +0600

    added consumes attribute

--- src/main/java/kg/bank/payments/controller/PaymentController.java
+++ src/main/java/kg/bank/payments/controller/PaymentController.java
@@ -18,14 +18,9 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
-    /**
-     *
-     * request Должен прийти только на один метод
-     * Надо будет объединить оба метода в один
-     * Разделение пойдет согласно QE11, QE10
-     */
-
-    @PostMapping(value = "/pay", produces = {"application/xml"})
+    @PostMapping(value = "/pay",
+                produces = {"application/xml"},
+                consumes = {"application/xml"})
     public ResponseEntity<XmlData> pay(@RequestBody XmlData request) {
         XmlData response = paymentService.execute(request);
         return ResponseEntity.ok(response);
